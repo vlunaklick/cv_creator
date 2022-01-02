26ecdebe47cfb9c1a6f8bacffab5ef974e13ddb3
Valen <v.lunaklick@gmail.com>
2022-01-02 19:25:36 -0300

    Changed some colors

--- src/components/InputText/InputText.js
+++ src/components/InputText/InputText.js
@@ -1,13 +1,17 @@
 import React from 'react'
 import './inputtext.css'
 
-const InputText = (props) => {
-
-    return (
-        <div>
-            <input onChange={props.opcion} className="inputL" type="text" placeholder={props.placeholder} />
-        </div>
-    )
+const InputText = props => {
+	return (
+		<>
+			<input
+				onChange={props.opcion}
+				className='inputL'
+				type='text'
+				placeholder={props.placeholder}
+			/>
+		</>
+	)
 }
 
 export default InputText
--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,86 +1,87 @@
 import React from 'react'
 import './sidebar.css'
 
-function Sidebar(props){
-    let name = props.name.toUpperCase()
-    let location,email,phone,nombre;
-    if (props.location !== ""){
-        location = (
-            <div className="mailS">
-                <i class="fas fa-map-marker-alt textS"></i>
-                <p className="textS textSS">{props.location}</p>
-            </div>
-        )
-    }
+function Sidebar(props) {
+	let name = props.name.toUpperCase()
+	let location, email, phone, nombre
+	if (props.location !== '') {
+		location = (
+			<div className='mailS'>
+				<i className='fas fa-map-marker-alt textS'></i>
+				<p className='textS textSS'>{props.location}</p>
+			</div>
+		)
+	}
 
-    if (props.phone !== ""){
-        phone = (
-            <div className="mailS">
-                <i class="fas fa-phone-square textS"></i>
-                <p className="textS textSS">{props.phone}</p>
-            </div>
-        )
-    }
+	if (props.phone !== '') {
+		phone = (
+			<div className='mailS'>
+				<i className='fas fa-phone-square textS'></i>
+				<p className='textS textSS'>{props.phone}</p>
+			</div>
+		)
+	}
 
-    if (props.email !== ""){
-        email = (
-            <div className="mailS">
-                <i className="fas fa-envelope textS"></i>
-                <p className="textS textSS">{props.email}</p>
-            </div>
-        )
-    }
+	if (props.email !== '') {
+		email = (
+			<div className='mailS'>
+				<i className='fas fa-envelope textS'></i>
+				<p className='textS textSS'>{props.email}</p>
+			</div>
+		)
+	}
 
-    let suma;
-    
-    if (props.email !== "" || props.phone !== "" || props.location !== ""){
-        suma = (
-            <div className="contactsStuff">
-                {email}
-    
-                {phone}
-                
-                {location}
-    
-            </div>
-        )
-    }
+	let suma
 
-    let foto;
+	if (props.email !== '' || props.phone !== '' || props.location !== '') {
+		suma = (
+			<div className='contactsStuff'>
+				{email}
 
-    if (props.showPro === true){
-        foto = (
-            <div className="profilePic">
-                <img className="profilePicture" src="https://www.infoprision.com/sites/default/files/fg-avatar-anonymous-user-retina.png" alt="" />
-            </div> 
-        );
-        nombre = (
-            <div className="basicStuff">
-                <h1 className="nameProf">{name}</h1>
-                <p className="titProf"> {props.title}</p>
-            </div>
-        );
-    } else {
-        foto = "";
-        nombre = (
-            <div className="basicStuff">
-                <h1 className="nameProfG">{name}</h1>
-                <p className="titGrande"> {props.title}</p>
-            </div>
-        );
-    }
+				{phone}
 
+				{location}
+			</div>
+		)
+	}
 
-        return (
-            <div className="sidebar">
-                {foto}
-                
-                {nombre}
+	let foto
 
-                {suma}
-            </div>
-        )
+	if (props.showPro === true) {
+		foto = (
+			<div className='profilePic'>
+				<img
+					className='profilePicture'
+					src='https://www.infoprision.com/sites/default/files/fg-avatar-anonymous-user-retina.png'
+					alt=''
+				/>
+			</div>
+		)
+		nombre = (
+			<div className='basicStuff'>
+				<h1 className='nameProf'>{name}</h1>
+				<p className='titProf'> {props.title}</p>
+			</div>
+		)
+	} else {
+		foto = ''
+		nombre = (
+			<div className='basicStuff'>
+				<h1 className='nameProfG'>{name}</h1>
+				<p className='titGrande'> {props.title}</p>
+			</div>
+		)
+	}
+
+	return (
+		<div className='sidebar'>
+			{foto}
+
+			{nombre}
+
+			{suma}
+		</div>
+	)
 }
-
 
 export default Sidebar
